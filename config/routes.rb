73608ed581f1ceafe2e8dbbaecc205d33a9cb37e
Rails Udemy Course <opacic.milos_95@hotmail.com>
2018-03-07 18:30:43 +0100

    Setup email functionality for sing-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  default_url_options :host => "http://localhost:3000"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
